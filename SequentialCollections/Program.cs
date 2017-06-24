ea243aef35653e1af9162cb7ac9c4019d469e821
psoltesz <peter.soltesz92@gmail.com>
2017-06-24 19:30:02 +0200

    tasks done, only publishing the first repo

--- SequentialCollections/Program.cs
+++ SequentialCollections/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +10,38 @@ namespace SequentialCollections
     {
         static void Main(string[] args)
         {
+            Queue queue = new Queue();
+            queue.Enqueue("first");
+            queue.Enqueue("second");
+            queue.Enqueue("third");
+            queue.Enqueue("fourth");
+            queue.Enqueue("fifth");
+
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+            stack.Push("Fifth");
+
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+            }
+
+            Console.ReadKey();
         }
     }
 }
